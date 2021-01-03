e35d09c22f5c8e493abcecd863c8941484108e8a
martin.mueller <m.mueller@unimed.de>
2021-01-03 17:07:06 +0100

    feat: add expected pipeline json for unit tests

--- src/pipeline-stack.ts
+++ src/pipeline-stack.ts
@@ -129,13 +129,8 @@ export class PipelineStack extends core.Stack {
       new core.CfnOutput(customStage.customStack, 'Stage', { value: stageAccount.stage || 'not set!' });
       new core.CfnOutput(customStage.customStack, 'CommitID', { value: process.env.CODEBUILD_RESOLVED_SOURCE_VERSION || 'not set!' });
       new core.CfnOutput(customStage.customStack, 'RepoUrl', { value: `https://github.com/${props.gitHub.owner}/${props.repositoryName}` || 'not set!' });
-      // new core.CfnOutput(customStage.customStack, 'WebhookTrigger', { value: process.env.CODEBUILD_WEBHOOK_TRIGGER || 'not set!' });
       new core.CfnOutput(customStage.customStack, 'BranchName', { value: props.branch || 'not set!' });
-      // new core.CfnOutput(customStage.customStack, 'CommitUrl', { value: repo.variables.commitUrl || 'not set!' });
-      // new core.CfnOutput(customStage.customStack, 'CommitterDate', { value: repo.variables.committerDate || 'not set!' });
-      // new core.CfnOutput(customStage.customStack, 'CommitMessage', { value: repo.variables.commitMessage || 'not set!' });
-      // new core.CfnOutput(customStage.customStack, 'RepositoryName', { value: repo.variables.repositoryName || 'not set!' });
-      // new core.CfnOutput(customStage.customStack, 'AuthorDate', { value: repo.variables.authorDate || 'not set!' });
+      new core.CfnOutput(customStage.customStack, 'BuildUrl', { value: process.env.CODEBUILD_BUILD_URL || 'not set!' });
 
       const preprodStage = cdkPipeline.addApplicationStage(customStage, {
         manualApprovals: props.manualApprovals?.call(this, stageAccount),
