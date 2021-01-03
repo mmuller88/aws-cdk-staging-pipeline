1d2658ed1b65204cddfd5067a8426335f6ac08c4
martin.mueller <m.mueller@unimed.de>
2021-01-03 12:11:03 +0100

    feat: add snapshot capability for unit tests

--- test/integ.snapshot.test.ts
+++ test/integ.snapshot.test.ts
@@ -5,7 +5,6 @@ import { IntegTesting } from './integ.default';
 test('integ snapshot validation', () => {
   const integ = new IntegTesting();
   integ.stack.forEach((stack) => {
-    // ATM probably not possible to create a snapshot as it uses a newer bootstrap. I get that snapshot manually from the AWS Console
     expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
   });
 });
