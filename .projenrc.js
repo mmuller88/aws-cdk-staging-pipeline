2edd862e498fd79cfa2c0beb7c7aeb97e36a06ef
Martin Mueller <damadden88@googlemail.com>
2021-05-07 12:50:13 -0300

    add version bump

--- .projenrc.js
+++ .projenrc.js
@@ -1,6 +1,6 @@
 const { TypeScriptProject } = require('projen');
 
-const cdkVersion = '1.97.0';
+const cdkVersion = process.env.CDK_VERSION || '1.97.0';
 
 const deps = [
   // '@types/aws-lambda',
