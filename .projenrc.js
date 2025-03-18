d47e2dd1e7f9ba40eea5f2a6d5f00d98a29e15cd
Martin Mueller <damadden88@googlemail.com>
2025-03-18 11:07:59 +0000

    fix: update projen version

--- .projenrc.js
+++ .projenrc.js
@@ -1,53 +1,30 @@
-const { TypeScriptProject } = require('projen');
+const { typescript } = require('projen');
 
 const cdkVersion = process.env.CDK_VERSION || '1.122.0';
 
-const deps = [
-  // '@types/aws-lambda',
-  // 'aws-lambda',
-  // 'aws-sdk',
-  // 'esbuild@^0',
-  // `@aws-cdk/assert@${cdkVersion}`,
-];
-
-const cdkDeps = [
-  // '@mobileposse/auto-delete-bucket',
-  `@aws-cdk/core@${cdkVersion}`,
-  `@aws-cdk/aws-codepipeline@${cdkVersion}`,
-  `@aws-cdk/aws-iam@${cdkVersion}`,
-  `@aws-cdk/aws-s3@${cdkVersion}`,
-  `@aws-cdk/aws-codepipeline-actions@${cdkVersion}`,
-  `@aws-cdk/pipelines@${cdkVersion}`,
-  `@aws-cdk/assert@${cdkVersion}`,
-  // `@aws-cdk/aws-lambda-nodejs@${cdkVersion}`,
-  // 'aws-cdk-build-badge',
-];
-
-const project = new TypeScriptProject({
+const project = new typescript.TypeScriptProject({
   authorAddress: 'damadden88@googlemail.de',
   authorName: 'martin.mueller',
   defaultReleaseBranch: 'master',
   name: 'aws-cdk-staging-pipeline',
   repositoryUrl: 'https://github.com/mmuller88/aws-cdk-staging-pipeline',
   projenUpgradeSecret: 'PROJEN_GITHUB_TOKEN',
-  devDeps: [...deps, ...cdkDeps],
-  deps: deps,
-  peerDeps: cdkDeps,
-  bundledDeps: deps,
+
+  // CDK specific configurations
+  cdkVersion: cdkVersion,
+  deps: ['@mobileposse/auto-delete-bucket', 'aws-cdk-build-badge'],
+
+  // Release configurations
   releaseToNpm: true,
-  // python: {
-  //   distName: 'aws-cdk-staging-pipeline',
-  //   module: 'aws_cdk_staging_pipeline',
-  // },
-  keywords: ['cdk', 'aws', 'pipeline', 'staging'],
   majorVersion: 2,
   releaseBranches: {
     'aws-cdk-1-122-0': {
       majorVersion: 1,
       minorVersion: 122,
-      // patchVersion: 1,
     },
   },
+
+  keywords: ['cdk', 'aws', 'pipeline', 'staging'],
 });
 
 project.setScript('deploy', 'cdk deploy');
